c2191a686811d3df21637e641bc56b962169b412
hhuang2018 <hwangtiger@gmail.com>
2018-04-04 20:23:30 -0500

    bug fix: np.arry stack

--- scripts/plink_file_convert.py
+++ scripts/plink_file_convert.py
@@ -22,7 +22,7 @@ import numpy as np
 #output = '../Data/'
 
 plink_fp = "/home/hhuang/efs/GWASH_IMPUTED_DATA/ImputeQC/"
-metadata_fp = "/home/hhuang/data/available_cases_HLA.csv"
+metadata_fp = "/home/hhuang/data/available_cases.csv"
 output = '/home/hhuang/data/BMT_mm/'
 
 #parser = argparse.ArgumentParser()
@@ -48,12 +48,14 @@ for chrom in chrList:
             bmt_fams = fam.query("iid in ['" + metadata_avail_cases['NMDP_DID'][case_index] + 
                                            "', '" + 
                                            metadata_avail_cases['NMDP_RID'][case_index]+"']")
-            gt = bed[bmt_fams.i.values, :].compute()
-            bmt_gt = abs(gt[[0]] - gt[[1]])
+            gt = bed[:, bmt_fams.i.values].compute()
+            #bmt_gt = abs(gt[[0]] - gt[[1]])
+            bmt_gt = abs(gt[:, 0] - gt[:,1])
             if BMT_mm_table.shape[0] == 0:
                 BMT_mm_table = bmt_gt
             else:
-                BMT_mm_table = np.concatenate((BMT_mm_table, bmt_gt), axis =0)
+                # BMT_mm_table = np.concatenate((BMT_mm_table, bmt_gt), axis =0)
+                BMT_mm_table = np.stack((BMT_mm_table, bmt_gt))
         np.savez(output+'BMT_mm_table_chr_'+str(chrom)+'.npz', mm_table = BMT_mm_table, ID_table = metadata_avail_cases)
     except FileNotFoundError as e:
         print(e)
